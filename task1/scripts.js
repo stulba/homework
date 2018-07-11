08a6e3035f412b1aef53c6de1c1399ff2453afc1
Siarhei Stulba <siarhei.stulba@gmail.com>
2018-07-12 00:52:25 +0300

    Added eslint config

--- task1/scripts.js
+++ task1/scripts.js
@@ -2,50 +2,46 @@
 
 // Checks if a string contains a Number
 function containsNumber(str) {
-  let result = false
-  
-  for (let i = 0; i < str.length; i++) {
-    if (!isNaN(str[i])) {
-      result = true
-    }
-  }
+	let result = false;
 
-  return result
+	for (let i = 0; i < str.length; i++) {
+		if (!isNaN(str[i])) {
+			result = true;
+		}
+	}
+
+	return result;
 }
 
 // Reverse a string
 function reverseString(str) {
-  return str.split('').reverse().join('')
+	return str.split('').reverse().join('');
 }
 
 // Capitalizing every second letter in a string
-function everySecondToUpperCase(str) {
-  let result = ''
+function toUpperCase(str) {
+	let result = '';
 
-  for (let i = 0; i < str.length; i++) {
-    if (i % 2 === 0) {
-      result += str[i].toUpperCase()
-    } else {
-      result += str[i]
-    }
-  }
+	for (let i = 0; i < str.length; i++) {
+		if (i % 2 === 0) {
+			result += str[i].toUpperCase();
+		} else {
+			result += str[i];
+		}
+	}
 
-  return result
+	return result;
 }
 
-
-
-
-
-const userName = prompt('Please, enter your name', '')
+const userName = prompt('Please, enter your name', '');
 
 // Handle User Name
 function handleUserName(name) {
-  if (containsNumber(name)) {
-    return everySecondToUpperCase(name)
-  }
+	if (containsNumber(name)) {
+		return toUpperCase(name);
+	}
 
-  return reverseString(name)
+	return reverseString(name);
 }
 
-document.write(handleUserName(userName))
+document.write(handleUserName(userName));
--- task2/scripts.js
+++ task2/scripts.js
@@ -1,38 +1,38 @@
-const btn1 = document.querySelector('.js-get')
-const btn2 = document.querySelector('.js-show-image')
-const btn3 = document.querySelector('.js-create-human')
+const btn1 = document.querySelector('.js-get');
+const btn2 = document.querySelector('.js-show-image');
+const btn3 = document.querySelector('.js-create-human');
 
 // Go to Google
-btn1.onclick = function() {
-  window.location = 'https://www.google.com/'
-}
+btn1.onclick = function () {
+	window.location = 'https://www.google.com/';
+};
 
 // Show image
-btn2.onclick = function() {
-  document.write('<img src="./image.jpg" alt="Red cat" />')
-}
+btn2.onclick = function () {
+	document.write('<img src="./image.jpg" alt="Red cat" />');
+};
 
 // Create and visualise Human object
-btn3.onclick = function() {
-  const human = {
-    firstname: 'Siarhei',
-    lastname: 'Stulba',
-    email: 'placeholder@mail.com',
-    age: 13,
-    married: false,
-  }
+btn3.onclick = function () {
+	const human = {
+		firstname: 'Siarhei',
+		lastname: 'Stulba',
+		email: 'placeholder@mail.com',
+		age: 13,
+		married: false,
+	};
 
-  const list = document.createElement('ul')
+	const list = document.createElement('ul');
 
-  for (let key in human) {
-    const value = human[key]
-    const listItem = document.createElement('li')
+	for (let key in human) {
+		const value = human[key];
+		const listItem = document.createElement('li');
 
-    listItem.textContent = `${key}: ${value}`
-    list.appendChild(listItem)
+		listItem.textContent = `${key}: ${value}`;
+		list.appendChild(listItem);
 
-    console.log(`${key}: ${value} `)
-  }
+		console.log(`${key}: ${value} `); // eslint-disable-line no-console
+	}
 
-  document.body.appendChild(list)
-}
+	document.body.appendChild(list);
+};
